7ef877adcbaa824e51e18e428c0d522352edf653
Jacekas <J.Antulis@gmail.com>
2019-01-05 11:15:17 +0200

    Add new Item functionality complete

--- Todolis/TodolisViewController.swift
+++ Todolis/TodolisViewController.swift
@@ -8,7 +8,7 @@ import UIKit
 
 class TodolisViewController: UITableViewController {
 
-    let itemArray = ["Task 1", "Task 2", "Task 3"]
+    var itemArray = ["Task 1", "Task 2", "Task 3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +40,24 @@ class TodolisViewController: UITableViewController {
 
         tableView.deselectRow(at: indexPath, animated: true)
         
+    }
+ 
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todolis Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen once the user clicks the Add Item button on our UIAlert
+        self.itemArray.append(textField.text!)
+        self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     
